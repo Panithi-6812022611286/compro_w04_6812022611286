225e365b4390f992cc232952177e42603362dc54
Panithi-6812022611286 <s6812022611286@email.kmutnb.ac.th>
2025-07-15 16:11:08 +0700

    aaa

--- Coding_w04-03.c
+++ Coding_w04-03.c
@@ -1,3 +1,7 @@
-ddcddcdcd
+#include <stdio.h>  // Preprocessor directive (เรียกใช้ Libary)
 
-
+int main() {                         // Keyword: int, main
+    printf("Hello,\n");              // Function call: printf (Identifier), \n = New line
+    printf("C Tokens!\n");           // Constant string: "C Tokens!"
+    return 0;                        // Keyword: return, Constant: 0
+}
---
+++ Coding_w04-05.c
@@ -0,0 +1,7 @@
+#include <stdio.h>  // Preprocessor directive (เรียกใช้ Libary)
+
+int main() {                         // Keyword: int, main
+    printf("Hello,\n");              // Function call: printf (Identifier), \n = New line
+    printf("C Tokens!\n");           // Constant string: "C Tokens!"
+    return 0;                        // Keyword: return, Constant: 0
+}
